9d8b188b89d7c0af75213fb848183ad5b7b47ac1
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2019-12-02 00:16:10 +0530

    main memory usage history graph added

---
+++ main_mem_graph.py
@@ -0,0 +1,43 @@
+import curses
+from time import sleep
+import psutil
+
+
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.scrollok(True)
+    stdscr.idlok(True)
+    pad = stdscr
+    pad.nodelay(True)
+    pad.addstr(1, 2, 'MAIN MEMORY USAGE GRAPH', curses.A_DIM)
+
+    dq = [0] * 80  # deque of length 20 i,e. stores previous 20 values
+    # initials 4 and 30
+    while True:
+        per = psutil.virtual_memory().percent
+        pad.addstr(2, 2, 'Current Usage: ' + str(per) + '%')
+        dq.append(int((int(per) * 20) / 100))
+        # print(dq)
+        dq.pop(0)
+        prev = dq[0]
+        for row in range(4, 84):
+            # print(prev,dq[row-4])
+            for col in range(21):
+                if dq[row - 4] <= prev:
+                    if prev >= col >= dq[row - 4]:
+                        pad.addch(30 - col, row, "-")
+                    else:
+                        pad.addch(30 - col, row, ' ')
+                else:
+                    if prev <= col <= dq[row - 4]:
+                        pad.addch(30 - col, row, '-')
+                    else:
+                        pad.addch(30 - col, row, ' ')
+            prev = dq[row - 4]
+        pad.refresh()
+        sleep(1)
+        if pad.getch() > 0:
+            exit()
+
+
+curses.wrapper(main)
